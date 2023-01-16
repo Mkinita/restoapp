1e087cbeba7103d78a48cede857ed22f0a1e6c0a
unknown <jerez4959@gmail.com>
2023-01-16 18:28:08 -0300

    beta1.6

--- components/VentasDiarias.js
+++ components/VentasDiarias.js
@@ -7,9 +7,16 @@ import {formatearDinero} from "../helpers/index"
 const VentasDiarias = ({orden}) => {
     const {id, nombre, total, pedido, fecha} = orden
 
+
+    
+
   
   return (
-    <div className="flex border p-1 space-y-5">
+    <div className="border p-1 space-y-5">
+
+
+
+
             
             
           
@@ -17,21 +24,51 @@ const VentasDiarias = ({orden}) => {
         <div>
             {pedido.map(platillo => (
                 <div key={platillo.id}
-                className="py-3 flex border-b last-of-type:border-0 items-center"
+
+                
+                className="py-1 flex border-b last-of-type:border-0 items-center"
                 >
+
+                    <table class="w-full text-xs text-left">
+                                <thead class="text-xs uppercase">
+                                    <tr>
+                                    <th scope="col" className="w-1/3 p-1 text-center text-xs text-black">{platillo.nombre}</th>
+                                    <th  scope="col" className="w-1/3 p-1 text-center text-xs text-black">{nombre}</th>
+                                    <th  scope="col" className="w-1/4 p-1 text-center text-xs text-black">{total}</th>
+                                    </tr>
+                                </thead>
+
+
+                                
+                    </table>
+
                     
-                    <div className="flex-auto w-96">
+
+
+                    
+
+
+
+                    
+                                
+                    
+                    {/* <div className="flex-auto w-96">
                         <h4 className="text-s font-bold text-black text-left">{platillo.nombre}</h4>
                         
                     </div>
 
                     <div class="flex-auto w-64 text-s">{nombre}</div>
 
-                    <div class="flex-auto w-36 text-right text-s">{total}</div>
+                    
+
+                    <div className="w-1/4 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{total}</div> */}
                 </div>
             ))}
         </div>
 
+
+
+       
         
 
     </div>
@@ -39,3 +76,7 @@ const VentasDiarias = ({orden}) => {
 }
 
 export default VentasDiarias
+
+
+
+
--- pages/administrador.js
+++ pages/administrador.js
@@ -10,7 +10,12 @@ export default function ventas_diarias() {
     const fetcher = () => axios('/api/admin').then(datos => datos.data)
     const { data, error, isLoading } = useSWR('/api/admin',fetcher,{refreshInterval: 100} )
 
+    
+
     console.log(data)
+
+    
+
 
     
      
@@ -29,7 +34,7 @@ export default function ventas_diarias() {
             <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-200 dark:text-gray-400">
                                 <tr>
-                                <th scope="col" className="w-1/7 p-2 text-center text-xs text-black">Pido</th>
+                                <th scope="col" className="w-1/7 p-2 text-center text-xs text-black">Pedido</th>
                                 <th  scope="col" className="w-1/7 p-2 text-center text-xs text-black">Nombre</th>
                                 <th  scope="col" className="w-1/7 p-2 text-center text-xs text-black">Total</th>
                                 </tr>
@@ -47,9 +52,28 @@ export default function ventas_diarias() {
                     orden={orden}
                 />
 
-                ):<p>No Hay Ordenes Pendientes</p>}
+                ):<p className='text-center'>No Hay Ordenes Pendientes</p>}
+
+ 
+
+
+                <table class="w-full text-xs text-left">
+                        <thead class="text-xs uppercase">
+                            <tr>
+                                <th scope="col" className="w-1/3 p-1 text-center text-xs text-black"></th>
+                                <th  scope="col" className="w-1/3 p-1 text-center text-xs text-black">Total</th>
+                                <th  scope="col" className="w-1/4 p-1 text-center text-xs text-black">$10.000</th>
+                            </tr>
+                        </thead>
+                </table>
+
 
         </VentasDiariaLayout>
+
+
+
+
+        
 
 
 
